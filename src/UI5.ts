bab3184f16cba0798a142743928acd8b2a6b07d0
Leonardo Carreiro <leonardo.mcarreiro@gmail.com>
2017-10-08 12:08:43 -0300

    Added comments in the code

--- src/UI5.ts
+++ src/UI5.ts
@@ -1,5 +1,13 @@
-function UI5(name: string): any {
-    return function (target: FunctionConstructor, propertyKey: string, descriptor: PropertyDescriptor) {
+/**
+ * Convert a class definition to the UI5 format of inheritance. This decorator can only be used in a class that extends from
+ * another UI5 class. If your class doesn't extends from any other, don't use this decorator or make your class extend from
+ * sap.ui.base.Object
+ * @param name Full name of the class. This parameter will be passed to BaseClass.extend(name, ...) method at runtime.
+ */
+function UI5(name: string): Function
+{
+    return function (target: FunctionConstructor): FunctionConstructor
+    {
         var functionMembers: string[] = Object.getOwnPropertyNames(function () {});
         var staticMembers:   string[] = Object.getOwnPropertyNames(target).filter(o => functionMembers.indexOf(o) === -1);
         var instanceMethods: string[] = Object.getOwnPropertyNames(target.prototype);
