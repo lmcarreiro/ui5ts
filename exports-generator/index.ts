b66edff95ce1dac5fe72e37002ed9e0e4c793a62
Leonardo Carreiro <leonardo.mcarreiro@gmail.com>
2017-10-08 09:00:16 -0300

    making the web request on node

--- exports-generator/index.ts
+++ exports-generator/index.ts
@@ -1,2 +1,23 @@
 
-console.log("Hello World");
+import * as request from 'request';
+
+var namespaces = [
+    "https://sapui5.hana.ondemand.com/test-resources/sap/ui/core/designtime/api.json"
+];
+
+namespaces.forEach(url => {
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+    
+        if (!error && response.statusCode === 200) {
+            console.log(body) // Print the json response
+        }
+        else {
+            console.error(error);
+        }
+
+    })
+
+});
