0c2041e14003361e00fbabafe37d8816078c1e64
Leonardo Machado Carreiro <leonardo.mcarreiro@gmail.com>
2017-10-12 12:48:33 -0300

    increment types from ui5 api

--- src/generator/ui5api.ts
+++ src/generator/ui5api.ts
@@ -5,36 +5,87 @@ export interface API
     version: string;
 }
 
+export type Symbol = SymbolNamespace|SymbolClass|SymbolEnum|SymbolInterface;
+
+export enum Kind {
+    Namespace   = "namespace",
+    Class       = "class",
+    Enum        = "enum",
+    Interface   = "interface"
+}
+
 export interface SymbolBase
 {
-    basename: string;
-    description: string;
-    export: string;
-    methods: Method[];
-    module: string;
     name: string;
+    basename: string;
     resource: string;
-    visibility: "public"|"private";
+    module: string;
+    visibility: "public"|"protected"|"restricted";
+    description?: string;
+    export?: string;
+    static?: boolean;
+    since?: string;
 }
 
 export interface SymbolNamespace extends SymbolBase
 {
-    kind: "namespace";
+    kind: Kind.Namespace;
+
+    events?: Event[];
+    methods?: Method[];
+    properties?: Property[];
+    extends?: string;
+    deprecated?: boolean;
+    experimental?: boolean;
+    final?: boolean;
 }
 
 export interface SymbolClass extends SymbolBase
 {
-    kind: "class";
+    kind: Kind.Class;
+
+    events?: Event[];
+    methods?: Method[];
+    properties?: Property[];
     constructor: any;
-    extends: string;
-    implements: string[];
-    static: boolean;
-    ["ui5-metadata"]: any;
+    extends?: string;
+    implements?: string[];
+    abstract?: boolean;
+    deprecated?: boolean;
+    experimental?: boolean;
+    final?: boolean;
+    "ui5-metadata"?: any;
 }
 
-export type Symbol = SymbolNamespace|SymbolClass;
+export interface SymbolEnum extends SymbolBase
+{
+    kind: Kind.Enum;
+
+    properties?: Property[];
+    deprecated?: boolean;
+    experimental?: string;
+}
+
+export interface SymbolInterface extends SymbolBase
+{
+    kind: Kind.Interface;
+
+    events?: Event[];
+    methods?: Method[];
+}
 
 export interface Method
 {
     //TODO: fill this
 }
+
+export interface Property
+{
+    //TODO: fill this
+}
+
+export interface Event
+{
+    //TODO: fill this
+}
+
