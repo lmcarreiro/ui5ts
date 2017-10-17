87343448db2a634e68000b76cc7532f78ab13921
Leonardo Machado Carreiro <leonardo.carreiro@petrobras.com.br>
2017-10-17 19:16:46 -0200

    Rewrite printData method in a recursive way, to see all the API structure

--- src/generator/Generator.ts
+++ src/generator/Generator.ts
@@ -101,15 +101,15 @@ export default class Generator
         allSymbols.sort((a, b) => a.name.localeCompare(b.name));
 
         //TreeNode.createFromSymbolsArray<JqueryTreeNode>(JqueryTreeNode, allSymbols.filter(s => s.name.startsWith("jQuery.")), "jQuery", 0);
-        let jQueryTree = TreeNode.createFromSymbolsArray(JQuerySapTreeNode, allSymbols.filter(s => s.name.match(/^jQuery([.]|$)/)), null, 0)[0];
-        let jQueryOutput: string[] = [];
-        let tsJQuery = jQueryTree.generateTypeScriptCode(jQueryOutput);
-        this.createFile(this.baseDefinitionsPath + "jQuery.d.ts", jQueryOutput.join(""));
+        // let jQueryTree = TreeNode.createFromSymbolsArray(JQuerySapTreeNode, allSymbols.filter(s => s.name.match(/^jQuery([.]|$)/)), null, 0)[0];
+        // let jQueryOutput: string[] = [];
+        // let tsJQuery = jQueryTree.generateTypeScriptCode(jQueryOutput);
+        // this.createFile(this.baseDefinitionsPath + "jQuery.d.ts", jQueryOutput.join(""));
 
-        let sapTree = TreeNode.createFromSymbolsArray(SapTreeNode, allSymbols.filter(s => s.name.match(/^sap([.]|$)/)), null, 0)[0];
-        let sapOutput: string[] = [];
-        let tsSap = sapTree.generateTypeScriptCode(sapOutput);
-        this.createFile(this.baseDefinitionsPath + "sap.d.ts", sapOutput.join(""));
+        // let sapTree = TreeNode.createFromSymbolsArray(SapTreeNode, allSymbols.filter(s => s.name.match(/^sap([.]|$)/)), null, 0)[0];
+        // let sapOutput: string[] = [];
+        // let tsSap = sapTree.generateTypeScriptCode(sapOutput);
+        // this.createFile(this.baseDefinitionsPath + "sap.d.ts", sapOutput.join(""));
 
         this.printData(allSymbols);
     }
@@ -120,70 +120,65 @@ export default class Generator
      */
     private printData(symbols: ui5.Symbol[]): void
     {
-        var a: { [kind: string]: { [property: string]: number } } = {};
-        var enumPropertyProperties: { [property: string]: number } = {};
-        var classMethodProperties: { [method: string]: number } = {};
-        var classMethodParameterProperties: { [property: string]: number } = {};
-        var classMethodParameterPropertyProperties: { [property: string]: number } = {};
+        var result: { [name: string]: any } = {};
+        var object: { [name: string]: any[] } = {};
 
-        for (let s of symbols) {
-            a[s.kind] = a[s.kind] || {};
+        symbols.forEach(s => (object[s.kind] = object[s.kind] || []).push(s));
 
-            for (let p in s) {
-                a[s.kind][p] = a[s.kind][p] || 0;
-                a[s.kind][p]++;
+        this.addToResult(result, object);
 
+        console.log(result);
+    }
+
+    private addToResult(result: { [name: string]: any }, object: any): void
+    {
+        let storageValuesFrom = [
+            "kind",
+            "visibility",
+            "static",
+            "final",
+            "abstract",
+            "optional"
+        ];
+
+        if (Array.isArray(object)) {
+            if (object.length > 0 && typeof(object[0]) === "object") {
+                result.$length = (result.$length || 0) + object.length;
+                object.forEach(o => this.addToResult(result, o));
             }
-
-            if (s.kind === ui5.Kind.Enum) {
-                if (s.properties) {
-                    for (let property in s.properties) {
-                        for (let propertyProperty in s.properties[property]) {
-                            enumPropertyProperties[propertyProperty] = enumPropertyProperties[propertyProperty] || 0;
-                            enumPropertyProperties[propertyProperty]++;
-                        }
-                    }
-                }
-                else {
-                    //console.log(s.name);
-                }
+            else {
+                result.$examples = result.$examples || [];
+                if (result.$examples.length < 5) result.$examples.push(object);
             }
-
-            if (s.kind === ui5.Kind.Class) {
-                if (s.methods) {
-                    for (let method in s.methods) {
-                        for (let methodProperty in s.methods[method]) {
-                            classMethodProperties[methodProperty] = classMethodProperties[methodProperty] || 0;
-                            classMethodProperties[methodProperty]++;
-                        }
-                        let parameters = s.methods[method].parameters || [];
-                        for (let parameter in parameters) {
-                            for (let parameterProperty in parameters[parameter]) {
-                                classMethodParameterProperties[parameterProperty] = classMethodParameterProperties[parameterProperty] || 0;
-                                classMethodParameterProperties[parameterProperty]++;
-                            }
-                            let parameterProperties = parameters[parameter].parameterProperties || [];
-                            for (let parameterPropertyPropertyName in parameterProperties) {
-                                for (let parameterPropertyProperty in parameterProperties[parameterPropertyPropertyName]) {
-                                    classMethodParameterPropertyProperties[parameterPropertyProperty] = classMethodParameterPropertyProperties[parameterPropertyProperty] || 0;
-                                    classMethodParameterPropertyProperties[parameterPropertyProperty]++;
-                                }
-                            }
-                        }
-                    }
-                }
-                else {
-                    //console.log(s.name);
-                }
-            }
-
         }
+        else {
+            for (let key in object) {
+                if (object.hasOwnProperty(key)) {
+                    let value = object[key];
+                    key = key === "constructor" ? "_constructor" : key;
 
-        //console.log(a);
-        //console.log(enumPropertyProperties);
-        //console.log(classMethodProperties);
-        //console.log(classMethodParameterProperties);
-        //console.log(classMethodParameterPropertyProperties);
+                    result[key] = result[key] || { $count: 0 };
+                    result[key].$count++;
+
+                    if (typeof(value) === "object") {
+                        this.addToResult(result[key], value);
+                    }
+                    else {
+
+                        if (storageValuesFrom.indexOf(key) > -1) {
+                            result[key][value] = result[key][value] || 0;
+                            result[key][value]++;
+                        }
+                        else {
+                            result[key].$examples = result[key].$examples || [];
+                            if (result[key].$examples.length < 5 && result[key].$examples.indexOf(value) === -1) {
+                                result[key].$examples.push(value);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private createExports(apiList: ui5.API[]): void
