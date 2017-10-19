639bc783f68fea58b86b60f058a6d29713c1101a
Leonardo Machado Carreiro <leonardo.mcarreiro@gmail.com>
2017-10-19 16:49:08 -0200

    remove ui5 api symbol from Method tree node

--- src/generator/nodeTypes/Method.ts
+++ src/generator/nodeTypes/Method.ts
@@ -4,38 +4,48 @@ import TreeNode from "./base/TreeNode";
 
 export default class Method extends TreeNode {
 
+    private visibility: ui5.Visibility;
+    private static: boolean;
+    private name: string;
+    private description: string;
+    private parameters: ui5.Parameter[];
+    private returnValue: ui5.ReturnValueInfo;
 
     constructor(config: Config, apiSymbol: ui5.Method, indentationLevel: number) {
         super(config, indentationLevel);
 
-        this.content = apiSymbol;
+        this.visibility = super.replaceVisibility(apiSymbol.visibility);
+        this.static = apiSymbol.static || false;
+        this.name = apiSymbol.name;
+        this.description = apiSymbol.description || "";
+        this.parameters = apiSymbol.parameters || [];
+        this.returnValue = apiSymbol.returnValue || {};
+        this.returnValue.type = this.returnValue.type || "void";
     }
 
     public generateTypeScriptCode(output: string[]): void {
-        let method = this.content;
-
-        let visibilityModifier = method.visibility.replace(ui5.Visibility.Restricted, ui5.Visibility.Protected) + " ";
-        let staticModifier = method.static ? "static " : "";
-        let returnType = (method.returnValue && method.returnValue.type) || "void";
-        let parameters = (method.parameters || []).map(p => `${p.name.replace(/<[^>]+>/g, "")}${p.optional ? "?" : ""}: ${p.type}`);
+        let visibilityModifier = this.visibility.replace(ui5.Visibility.Restricted, ui5.Visibility.Protected) + " ";
+        let staticModifier = this.static ? "static " : "";
+        let parameters = (this.parameters || []).map(p => `${p.name.replace(/<[^>]+>/g, "")}${p.optional ? "?" : ""}: ${p.type}`);
 
         this.printTsDoc(output);
-        output.push(`${this.indentation}${visibilityModifier}${staticModifier}${method.name}(${parameters.join(", ")}): ${returnType};\r\n`);
+        output.push(`${this.indentation}${visibilityModifier}${staticModifier}${this.name}(${parameters.join(", ")}): ${this.returnValue.type};\r\n`);
     }
 
     protected printTsDoc(output: string[]): void {
-        let method = this.content;
         let docInfo: string[] = [];
 
-        (method.parameters || []).forEach(p => {
-            docInfo.push(`@param {${p.type}} ${p.name} - ${p.description}.`);
+        (this.parameters || []).forEach(p => {
+            docInfo.push(`@param {${p.type}} ${p.name} - ${p.description}`);
         });
 
-        if (method.returnValue) {
-            docInfo.push(`@returns {${method.returnValue.type}} ${method.returnValue.description}.`);
+        let returnType = this.returnValue.type ? `{${this.returnValue.type}}` : "";
+        let returnDescription = this.returnValue.description ? this.returnValue.description : "";
+        if (returnType !== "void") {
+            docInfo.push(`@returns ${returnType} ${returnDescription}`);
         }
 
-        super.printTsDoc(output, method.description, docInfo);
+        super.printTsDoc(output, this.description, docInfo);
     }
 
 }
