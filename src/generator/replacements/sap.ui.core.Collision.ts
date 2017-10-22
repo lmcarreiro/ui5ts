65e10a09c3ce858265aa81e25e4673938af03337
Leonardo Carreiro <leonardo.mcarreiro@gmail.com>
2017-10-22 14:15:28 -0200

    enum replacement sap.ui.core.Collision

---
+++ src/generator/replacements/sap.ui.core.Collision.ts
@@ -0,0 +1,14 @@
+export enum Collision {
+    Flip        = "flip",
+    FlipFlip    = "flip flip",
+    FlipFit     = "flip fit",
+    FlipNone    = "flip none",
+    Fit         = "fit",
+    FitFlip     = "fit flip",
+    FitFit      = "fit fit",
+    FitNone     = "fit none",
+    None        = "none",
+    NoneFlip    = "none flip",
+    NoneFit     = "none fit",
+    NoneNone    = "none none"
+}
