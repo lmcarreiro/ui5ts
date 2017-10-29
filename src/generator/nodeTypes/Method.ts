fc9839b71ddb4f5472e97b1fb3245088e17de4de
Leonardo Carreiro <leonardo.mcarreiro@gmail.com>
2017-10-29 14:22:48 -0200

    One TsDoc for each method overload

--- src/generator/nodeTypes/Method.ts
+++ src/generator/nodeTypes/Method.ts
@@ -34,7 +34,6 @@ export default class Method extends TreeNode {
     }
 
     public generateTypeScriptCode(output: string[]): void {
-        this.printTsDoc(output);
         this.printMethodOverloads(output, this.parameters || []);
         this.printCompatibilityMethodOverload(output);
     }
@@ -46,8 +45,15 @@ export default class Method extends TreeNode {
                 type: "any[]",
                 spread: true
             };
-            let comment = "This method overload is here just for compatibility with the overrided methods in base classes";
-            this.print(output, [new Parameter(this.config, symbol, this.fullName)], "any", comment);
+            let parameters = [new Parameter(this.config, symbol, this.fullName)];
+            let returnValue = { type: "any", description: "" };
+
+            let description = `This method overload is here just for compatibility reasons to avoid compiler errors,
+                because UI5 API doesn't follow all TypeScript method override rules.
+                Don't use it. If the definitions are wrong, please open an issue in https://github.com/lmcarreiro/ui5ts/issues instead.`;
+
+            this.printMethodTsDoc(output, description, parameters, returnValue);
+            this.print(output, parameters, returnValue.type);
         }
     }
 
@@ -65,10 +71,11 @@ export default class Method extends TreeNode {
             }
         }
 
+        this.printMethodTsDoc(output, this.description, parameters, this.returnValue);
         this.print(output, parameters, this.returnValue.type);
     }
 
-    private print(output: string[], parameters: Parameter[], returnType: string, comment?: string): void {
+    private print(output: string[], parameters: Parameter[], returnType: string): void {
         let declaration: string;
 
         switch (this.parentKind) {
@@ -87,21 +94,19 @@ export default class Method extends TreeNode {
                 throw new Error(`UI5 kind '${this.parentKind}' cannot have methods.`);
         }
 
-        comment = comment ? ` // ${comment}` : "";
-
         let parametersCode = parameters.map(p => p.getTypeScriptCode());
         returnType = this.name !== "constructor" ? `: ${returnType}` : "";
-        output.push(`${this.indentation}${declaration}${this.name}(${parametersCode.join(", ")})${returnType};${comment}\r\n`);
+        output.push(`${this.indentation}${declaration}${this.name}(${parametersCode.join(", ")})${returnType};\r\n\r\n`);
     }
 
-    protected printTsDoc(output: string[]): void {
-        let docInfo = (this.parameters || []).map(p => p.getTsDoc().replace(/\r\n|\r|\n/g, " "));
+    private printMethodTsDoc(output: string[], description: string, parameters: Parameter[], returnValue: { type: string, description: string }): void {
+        let docInfo = parameters.map(p => p.getTsDoc().replace(/\r\n|\r|\n/g, " "));
 
-        if (this.returnValue.type !== "void") {
-            docInfo.push(`@returns {${this.returnValue.type}} ${this.returnValue.description}`.replace(/\r\n|\r|\n/g, " "));
+        if (returnValue.type !== "void") {
+            docInfo.push(`@returns {${returnValue.type}} ${returnValue.description}`.replace(/\r\n|\r|\n/g, " "));
         }
 
-        super.printTsDoc(output, this.description, docInfo);
+        super.printTsDoc(output, description, docInfo);
     }
 
 }
